e67d1c87dd43951a6f1829df0dd46c148b2603d8
kumar_mano <ymanojkumar023@gmail.com>
2019-10-15 23:30:08 +0530

    test1

--- app.py
+++ app.py
@@ -180,10 +180,7 @@ def upload():
 
 '''
 if __name__ == '__main__':
-    # app.run(port=5001, debug=True)
-
     # Serve the app with gevent
     http_server = WSGIServer(('0.0.0.0', 5000), app)
     http_server.serve_forever()
-    
 '''
